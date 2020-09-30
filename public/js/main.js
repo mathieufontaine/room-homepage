a71b5ddfb46baa64797d9c984d08a8eb3540d900
Mathieu Fontaine <mathieu.antoine.fontaine@gmail.com>
2020-09-30 12:02:33 +0200

    responsive + overlay + js menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+// slider
+
 let prevBtn = document.querySelectorAll(".slider-prev");
 let nextBtn = document.querySelectorAll(".slider-next");
 
@@ -39,3 +41,32 @@ nextBtn.forEach(btn =>
     sliderContent[counter].classList.add("active");
   })
 );
+
+// menu toggler
+
+const hamburger = document.querySelector(".menu svg");
+const close = document.querySelector(".close");
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+const main = document.querySelector("main");
+const overlay = document.querySelector("#overlay");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.add("hide-mobile");
+  navbar.classList.remove("hide-mobile");
+  document.getElementById("overlay").style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  navbar.classList.add("hide-mobile");
+  menu.classList.remove("hide-mobile");
+  main.classList.remove("overlay");
+  overlay.style.display = "none";
+});
+
+overlay.addEventListener("click", () => {
+  navbar.classList.add("hide-mobile");
+  menu.classList.remove("hide-mobile");
+  main.classList.remove("overlay");
+  overlay.style.display = "none";
+});
